7f98b1533dba23416b67308316aa690d64d0ba11
shanthi <shanthijayasekar@gmail.com>
2014-02-04 20:25:47 -0800

    Initial Commit

---
+++ ios-todo-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-todo-app
+//
+//  Created by user on 2/4/14.
+//  Copyright (c) 2014 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
